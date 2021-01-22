d15036a757046bac38d1563d44db4a7bb2e27baa
shivam kumar <shivam.kr21dec@gmail.com>
2021-01-22 20:35:47 +0530

    host url added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-defined-32818',
     user : 'postgres',
     password : '',
     database : 'smart-brain'
