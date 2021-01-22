ce040a2ceefb8bfd4c37d62c1d4b3a101ff9ed8f
shivam kumar <shivam.kr21dec@gmail.com>
2021-01-22 20:38:32 +0530

    added database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : 'postgresql-defined-32818',
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
